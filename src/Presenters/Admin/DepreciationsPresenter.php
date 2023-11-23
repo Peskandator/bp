6dfed152b8bd722a104afc0c400f2ef681fcf0a0
Pučil <martin.pucil@seznam.cz>
2023-11-23 01:47:57 +0100

    Depreciations - overview by year, listing

--- src/Presenters/Admin/DepreciationsPresenter.php
+++ src/Presenters/Admin/DepreciationsPresenter.php
@@ -9,14 +9,13 @@ use App\Presenters\BaseAdminPresenter;
 
 final class DepreciationsPresenter extends BaseAdminPresenter
 {
-
     public function __construct(
     )
     {
         parent::__construct();
     }
 
-    public function actionDefault(?int $yearArg): void
+    public function actionDefault(?int $yearArg = null): void
     {
         $year = $yearArg;
         if (!$year) {
@@ -24,8 +23,10 @@ final class DepreciationsPresenter extends BaseAdminPresenter
             $year = (int)$today->format('Y');
         }
 
-        $this->template->depreciationsTax = $this->getTaxDepreciationsForYear($year);
-        $this->template->depreciationsAccounting = $this->getAccountingDepreciationsForYear($year);
+        $this->template->taxDepreciations = $this->getTaxDepreciationsForYear($year);
+        $this->template->accountingDepreciations = $this->getAccountingDepreciationsForYear($year);
+        $this->template->availableYears = $this->getAvailableYears();
+        $this->template->selectedYear = $year;
     }
 
     protected function getTaxDepreciationsForYear(int $year): array
@@ -62,4 +63,21 @@ final class DepreciationsPresenter extends BaseAdminPresenter
         return $matched;
     }
 
+    protected function getAvailableYears(): array
+    {
+        $availableYears = [];
+
+        $depreciations = $this->currentEntity->getTaxDepreciations();
+        /**
+         * @var DepreciationTax $depreciation
+         */
+        foreach ($depreciations as $depreciation) {
+            $depreciationYear = $depreciation->getYear();
+            if (!in_array($depreciationYear, $availableYears)) {
+                $availableYears[] = $depreciationYear;
+            }
+        }
+
+        return $availableYears;
+    }
 }
