5f242584e41a3b3b19b5b960c069f3f601c47219
Pučil <martin.pucil@seznam.cz>
2024-04-03 22:53:57 +0200

    Command - rename files 2

--- src/Console/Command/RenameFilesCommand.php
+++ src/Console/Command/RenameFilesCommand.php
@@ -36,37 +36,119 @@ class RenameFilesCommand extends Command
     {
         $root = dirname(__DIR__) . '\\Command';
 
-        $this->renameFilesInFolder(
-              $root,
-            '1654 BR 14 - Kraj Hradecký III',
-            1,
-            '1654 BR 14 - Kraj Hradecký',
-            []
-        );
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1654 Berní rula 7 - Kraj Bechyňský III',
+//            871,
+//            '1654 BR 7 - Kraj Bechyňský',
+//            []
+//        );
 
-        $this->renameFilesInFolder(
-            $root,
-            '1654 BR 15 - Kraj Hradecký IV',
-            1,
-            '1654 BR 15 - Kraj Hradecký',
-            []
-        );
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1654 Berní rula 7 - Kraj Bechyňský III doplnění',
+//            1031,
+//            '1654 BR 7 - Kraj Bechyňský',
+//            []
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1654 Berní rula 24 - Kraj Plzeňský II',
+//            441,
+//            '1654 BR 24 - Kraj Plzeňský',
+//            []
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1654 Berní rula 25 - Kraj Plzeňský III',
+//            727,
+//            '1654 BR 25 - Kraj Plzeňský',
+//            [],
+//            true
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1654 Berní rula 26 - Kraj Podbrdský',
+//            1,
+//            '1654 BR 26 - Kraj Podbrdský',
+//            []
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1654 Berní rula 34 - Kladsko',
+//            1,
+//            '1654 BR 34 - Kladsko',
+//            []
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1915 Chytilův úplný adresář království českého\\2024_03_11_16_39_12',
+//            7,
+//            '1915 Chytilův adresář',
+//            [49, 63, 129, 225, 321, 417, 513, 609, 705, 801, 897],
+//            true
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1915 Chytilův úplný adresář království českého\\2024_03_12_17_00_00',
+//            977,
+//            '1915 Chytilův adresář',
+//            [1025, 1121, 1217, 1281],
+//            true
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\1915 Chytilův úplný adresář království českého\\2024_03_13_15_51_55',
+//            1403,
+//            '1915 Chytilův adresář',
+//            [1409, 1537],
+//            true
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\2001 30-ti letá válka',
+//            7,
+//            '2001 30-ti letá válka',
+//            []
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\2001 Řepice 750 let',
+//            7,
+//            '2001 Řepice 750 let',
+//            [97, 99]
+//        );
+
+//        $this->renameFilesInFolder(
+//              $root,
+//            '1654 BR příprava\\2020 Sabath - Čech v americkém kongresu',
+//            9,
+//            '2020 Sabath Adolf',
+//            []
+//        );
+
 
         return 0;
     }
 
-    protected function renameFilesInFolder($rootFolder, $folderName, $namingCounterStart, $fileNaming, array $missing): void
+    protected function renameFilesInFolder($rootFolder, $folderName, $namingCounterStart, $fileNaming, array $missing, bool $fourZeros = false): void
     {
         $path = $rootFolder . '\\' . $folderName;
-        $files = array_diff(scandir($path), array('.', '..'));
+        $files = array_diff(scandir($path), array('.', '..', 'nepř'));
+
+        //var_dump($files);
 
         $namingCounter = $namingCounterStart;
-        $first = true;
         foreach ($files as $file) {
-            if ($first) {
-                $first = false;
-                continue;
-            }
             $pageNumber = $namingCounter;
             $fileName = $fileNaming . ' ';
             if ($pageNumber < 10) {
@@ -75,6 +157,11 @@ class RenameFilesCommand extends Command
             if ($pageNumber < 100) {
                 $fileName .= '0';
             }
+
+            if ($fourZeros && $pageNumber < 1000) {
+                $fileName .= '0';
+            }
+
             $fileName .= $pageNumber . '.jpg';
 
             $path = $rootFolder . '\\' . $folderName . '\\' . $file;
@@ -82,9 +169,9 @@ class RenameFilesCommand extends Command
 
             if (file_exists($path)) {
                 rename($path, $finalPath);
-                $namingCounter += 2;
                 var_dump($path);
                 var_dump($finalPath);
+                $namingCounter += 2;
             }
             if (in_array($namingCounter, $missing)) {
                 $namingCounter += 2;
