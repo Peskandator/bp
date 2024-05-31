0e64f866ee5eda22036cfb5bfdb892323146522d
Pučil <martin.pucil@seznam.cz>
2024-05-31 22:44:06 +0200

    Tables - export depreciations to xlsx

--- src/Presenters/Admin/DepreciationsPresenter.php
+++ src/Presenters/Admin/DepreciationsPresenter.php
@@ -8,6 +8,7 @@ use App\Odpisy\Components\EditDepreciationCalculator;
 use App\Odpisy\Forms\EditAccountingDepreciationFormFactory;
 use App\Odpisy\Forms\EditTaxDepreciationFormFactory;
 use App\Presenters\BaseAccountingEntityPresenter;
+use App\Utils\PhpXlsxGenerator;
 use Nette\Application\UI\Form;
 
 final class DepreciationsPresenter extends BaseAccountingEntityPresenter
@@ -64,5 +65,72 @@ final class DepreciationsPresenter extends BaseAccountingEntityPresenter
     {
         $form = $this->editAccountingDepreciationFormFactory->create($this->currentEntity);
         return $form;
+    }
+
+    public function actionExport(?int $yearArg = null, string $type = "tax"): void
+    {
+        $year = $yearArg;
+        if (!$year) {
+            $today = new \DateTimeImmutable('today');
+            $year = (int)$today->format('Y');
+        }
+
+        $depreciations = $this->currentEntity->getTaxDepreciationsForYear($year);
+        $fileName = $this->currentEntity->getName() . ' - Daňové odpisy ' . $year . '.xlsx';
+        if ($type === "accounting") {
+            $depreciations = $this->currentEntity->getAccountingDepreciationsForYear($year);
+            $fileName = $this->currentEntity->getName() . ' - Účetní odpisy ' . $year . '.xlsx';
+        }
+
+
+        $firstRow = [
+            'Majetek',
+            'Typ',
+            'Odp.sk., způsob',
+            'Rok odpisu',
+            'VC',
+            'Zvýš. VC',
+            'Sazba',
+            '%',
+            'Odpis',
+            'Oprávky',
+            'ZC',
+            'Provést',
+            'Provedeno',
+        ];
+
+        $rows = $this->getDataForExport($depreciations);
+        $xlsxFile = PhpXlsxGenerator::fromArray($firstRow, $rows);
+        $xlsxFile->downloadAs($fileName);
+    }
+
+    protected function getDataForExport(array $depreciations): array
+    {
+        $rows = [];
+
+        foreach ($depreciations as $depreciation) {
+            $asset = $depreciation->getAsset();
+            $taxGroup = $asset->getDepreciationGroupTax();
+            $taxGroupName = $taxGroup ? $taxGroup->getFullName() : '';
+
+            $row = [];
+            $row[] = $asset->getName();
+            $row[] = $asset->getAssetType()->getName();
+            $row[] = $taxGroupName;
+            $row[] = $depreciation->getDepreciationYear();
+            $row[] = $depreciation->getEntryPrice();
+            $row[] = $depreciation->getIncreasedEntryPrice();
+            $row[] = $depreciation->getRate();
+            $row[] = $depreciation->getPercentage();
+            $row[] = $depreciation->getDepreciationAmount();
+            $row[] = $depreciation->getDepreciatedAmount();
+            $row[] = $depreciation->getResidualPrice();
+            $row[] = $depreciation->isExecutable() ? 'ANO' : 'NE';
+            $row[] = $depreciation->isExecuted() ? 'ANO' : 'NE';
+
+            $rows[] = $row;
+        }
+
+        return $rows;
     }
 }
